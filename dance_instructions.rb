cd3a7f02ff8a02bade6ea44ad7d152ca32e30d4e
art123craft <cammiebluecammie@gmail.com>
2018-07-26 21:02:18 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -31,7 +31,9 @@ bob_the_reins
 def lasso
   puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
   puts "Lift right arm up and point right elbow diagonally to the right."
-  puts
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
+lasso
 
 # def skip_step("right")
 #   puts "Lower rightfoot to the ground."
