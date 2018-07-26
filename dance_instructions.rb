a34748ee11906e7cddbf673cdb0c77e41cde4a31
art123craft <cammiebluecammie@gmail.com>
2018-07-26 20:54:49 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -15,6 +15,11 @@ end
  skip_step("left")
 
 def bob_the_reins
+  puts "Holding your arms out in front of you, straight and at chest level."
+  puts "Cross your right wrist over your left and hold them together."
+  puts "Lift you arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end
+bob_the_reins 
 
 # def skip_step("right")
 #   puts "Lower rightfoot to the ground."
